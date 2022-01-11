dc2a0158deb70c80ae6bbd10966250fd5e9a0617
corud <corud1130@gmail.com>
2022-01-11 20:54:16 +0900

    app.py 수정 및 회원가입 부분 def 추가

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from pymongo import MongoClient
-import requests
-
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+SECRET_KEY = 'SPARTA'
 
-@app.route('/')
+import jwt
+
+from datetime import datetime, timedelta
+
+import hashlib
+
+# 페이지 접속-----------------------------
+@app.route('/', methods=['GET'])
 def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/register')
+def register():
     return render_template('register.html')
 
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
-
-
+# 회원가입-------------------------------
 @app.route("/addUser", methods=["POST"])
 def addUser():
-
-
     id = request.form['id']
     pw = request.form['pw']
     nickname = request.form['nickname']
@@ -36,17 +53,6 @@ def addUser():
     return jsonify({'msg': "입력."});
 
 
-
-
-
-# @app.route('/check_dup', methods=['POST'])
-# def check_dup():
-#     check_id = request.form['check_id']
-#     if db.userdb.find_one({'id': check_id}) is None:
-#         return jsonify({'msg': "사용 가능한 아이디 입니다."})
-#     else:
-#         return jsonify({'exists': "이미 존재하는 아이디 입니다."})
-
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     id = request.form['id']
@@ -54,14 +60,41 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
+# 로그인--------------------------------
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    return jsonify({'result': 'success'})
+
+@app.route('/api/nick', methods=['GET'])
+def api_valid():
+    return jsonify({'result': 'success'})
 
 
-#
-# @app.route("/deleteUser", methods=["POST"])
-# def deleteUser():
-#     id = request.form['id']
-#     db.deleteUser(id)
-#     return jsonify({'result': 'success'});
+
+# 메인---------------------------------------
+@app.route('/like', methods=['POST'])
+def like_star():
+    return jsonify({'result': 'success'})
+
+@app.route('/canclelike', methods=['POST'])
+def cancle_like():
+    return jsonify({'result': 'success'})
+
+@app.route('/del', methods=['POST'])
+def delete_star():
+    return jsonify({'result': 'success'})
+
+
+
+# 작성페이지---------------------------------
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
