3db99a3edf604f50e30fda6c0b022082a27ebe19
aaanjini <zini6633@gmail.com>
2022-01-12 18:40:05 +0900

    유저페이지에서 작성페이지로 이동구현, 프로필 변경 오류수정, 메인페이지 유저로 이동추가

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ def save_img():
         name_receive = request.form["name_give"]
         about_receive = request.form["about_give"]
         new_doc = {
-            "profile_name": name_receive,
+            "nickname": name_receive,
             "profile_info": about_receive
         }
         if 'file_give' in request.files:
